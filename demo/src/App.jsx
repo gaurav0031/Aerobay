c2cbad41a61dc624211645db239f1d42d89db1b5
gaurav0031 <gauravpratap2631@gmail.com>
2025-01-17 13:34:40 +0530

    Changes
    Few samall Changes

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
-import Neuro from "./Components/Neuro/Neuro"
+import Neuro from "./Components/Neuro/Neuro";
 import Spiral from "./Components/Spiral/Spiral";
-import Test from "./Components/Tap/Tap"
-import Reaction from "./Components/Reaction/Reaction";
-
-import Voice from "./Components/Voice/Voice";
 import Tap from "./Components/Tap/Tap";
+import Reaction from "./Components/Reaction/Reaction";
+import Voice from "./Components/Voice/Voice";
+
 
 
 const App = () => {
